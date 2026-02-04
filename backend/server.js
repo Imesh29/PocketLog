077c8a7d1f59b3fe35928678fef076aee0e8101a
Imesh29 <ridrajakaruna29@gmail.com>
2026-02-04 12:10:07 +0530

    Add Get and Delete route

--- backend/server.js
+++ backend/server.js
@@ -29,10 +29,62 @@ async function initDB() {
   }
 }
 
+app.get("/api/transactions/:userId", async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    const transactions =
+      await sql`SELECT * FROM transactions WHERE user_id = ${userId} ORDER BY created_at DESC`;
+
+    res.status(200).json(transactions);
+  } catch (error) {
+    console.log("Error getting the transaction", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
 app.post("/api/transactions", async (req, res) => {
   try {
-    const { title, amount, categiry, user_id } = req.body;
-  } catch (error) {}
+    const { title, amount, category, user_id } = req.body;
+
+    if (!title || !user_id || !category || amount === undefined) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const transaction = await sql`
+      INSERT INTO transactions(user_id,title,amount,category)
+      VALUES (${user_id},${title},${amount},${category})
+      RETURNING *
+    `;
+
+    console.log(transaction);
+    res.status(201).json(transaction[0]);
+  } catch (error) {
+    console.log("Error creating the transaction", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+app.delete("/api/transactions/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (isNaN(parseInt(id))) {
+      return res.status(400).json({ message: "Invalid transaction ID" });
+    }
+
+    const result =
+      await sql`DELETE FROM transactions WHERE id = ${id} RETURNING *`;
+
+    if (result.length === 0) {
+      return res.status(404).json({ message: "Transaction not found" });
+    }
+
+    res.status(200).json({ message: "Transactions deleted successfully!" });
+  } catch (error) {
+    console.log("Error deleting the transaction", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 });
 
 initDB().then(() => {
